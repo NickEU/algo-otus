9a7493a9b3f54ff3fd63fdc1629e91ace729b1f1
Nick <neverate@gmail.com>
2020-04-13 18:30:31 +0300

    + 0-str upd

--- 0-string/tester/Program.cs
+++ 0-string/tester/Program.cs
@@ -8,6 +8,7 @@ namespace tester
         static void Main()
         {
             StringLength strLenTask = new StringLength();
+            // the path assumes running via debugging in visual studio
             string pathToStrLenTestDir = Path.GetFullPath(Path.Combine(
                 Environment.CurrentDirectory, "..", "..", "..", "..", @"tests\0-string"));
             Tester strLenTester = new Tester(strLenTask, pathToStrLenTestDir);
